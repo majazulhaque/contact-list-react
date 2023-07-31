42c0e0ef6218dd8f999821c287a540c758cbeea1
Majaz <majazulhaque123@gmail.com>
2023-07-31 14:19:29 +0530

    error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import { Route, Routes } from 'react-router-dom';
 
@@ -11,24 +11,21 @@ import { useDispatch } from "react-redux";
 const App = () => {
     const dispatch = useDispatch();
     useEffect(() => {
-        const data = [];
         const promise = async () => {
-            await fetch('https://jsonplaceholder.typicode.com/users/')
-                .then((response) => response.json())
-                .then((json) => {
-                    json.map((contact) => {
-                        data.push({
-                            id: contact.id,
-                            name: contact.name,
-                            number: contact.phone,
-                            email: contact.email
-                        });
-                    })
-                });
+            const response = await fetch('https://jsonplaceholder.typicode.com/users/');
+            const json = await response.json();
+            const data = json.map((contact) => { // Added the return statement here
+                return {
+                    id: contact.id,
+                    name: contact.name,
+                    number: contact.phone,
+                    email: contact.email
+                };
+            });
             dispatch({ type: 'FETCH_CONTACTS', payload: data });
         };
         promise();
-    }, []);
+    }, [dispatch]);
 
 
     return (
@@ -36,15 +33,9 @@ const App = () => {
             <ToastContainer />
             <Navbar />
             <Routes>
-                <Route exact path="/" element={<Home />}>
-
-                </Route>
-                <Route path="/add" element={<AddContact />}>
-
-                </Route>
-                <Route path="/edit/:id" element={<EditContact />}>
-
-                </Route>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/add" element={<AddContact />} />
+                <Route path="/edit/:id" element={<EditContact />} />
             </Routes>
         </div>
     );
